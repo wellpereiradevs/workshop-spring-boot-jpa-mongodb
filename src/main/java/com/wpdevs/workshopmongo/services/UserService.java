01651c22aa75ee1e4f034f4321d8c099b4c530b5
wellpereiradevs <wellpereiradevs@gmail.com>
2023-12-06 16:23:35 -0300

    Inserção de usuário com POST

--- src/main/java/com/wpdevs/workshopmongo/services/UserService.java
+++ src/main/java/com/wpdevs/workshopmongo/services/UserService.java
@@ -1,5 +1,6 @@
 package com.wpdevs.workshopmongo.services;
 
+import com.wpdevs.workshopmongo.DTO.UserDTO;
 import com.wpdevs.workshopmongo.domain.User;
 import com.wpdevs.workshopmongo.exceptions.ObjectNotFoundException;
 import com.wpdevs.workshopmongo.repositories.UserRepository;
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -27,4 +27,11 @@ public class UserService {
         return user;
     }
 
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
 }
