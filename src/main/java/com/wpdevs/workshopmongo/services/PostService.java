3bf5e182ff95e058208cdd6d092f909b35c59a50
wellpereiradevs <wellpereiradevs@gmail.com>
2023-12-07 11:32:17 -0300

    Obtendo um (post) por id

---
+++ src/main/java/com/wpdevs/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.wpdevs.workshopmongo.services;
+
+import com.wpdevs.workshopmongo.domain.Post;
+import com.wpdevs.workshopmongo.exceptions.ObjectNotFoundException;
+import com.wpdevs.workshopmongo.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public List<Post> findAll() {
+        return repository.findAll();
+    }
+
+    public Post findById(String id) {
+        Post user = repository.findById(id).orElse(null);
+        if (user == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user;
+    }
+}
+
