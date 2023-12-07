f03224b5e491297ba062a0c3dd85c82b89facf54
wellpereiradevs <wellpereiradevs@gmail.com>
2023-12-07 15:53:29 -0300

    Consulta com vários critérios

--- src/main/java/com/wpdevs/workshopmongo/services/PostService.java
+++ src/main/java/com/wpdevs/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.wpdevs.workshopmongo.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -28,6 +29,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
 
